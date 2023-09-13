0bc647db6f471a379ab6bcc47a896622eb81c901
Ivan Pavlovic <pavlovic.ivan.26@gmail.com>
2023-09-13 16:11:20 +0200

    [JENKINS-69385] Add logging verbosity configuration (#21)
    * Add logging level verbosity logic

--- src/main/java/hudson/plugins/mstest/MsTestLogger.java
+++ src/main/java/hudson/plugins/mstest/MsTestLogger.java
@@ -9,9 +9,18 @@ class MsTestLogger implements Serializable {
 
     private static String prefix = "[MSTEST-PLUGIN]";
     private TaskListener listener;
+    private Level configuredLevel;
+    public static String HUDSON_PLUGINS_MSTEST_LEVEL = "hudson.plugins.mstest.level";
+    private static String ERROR_LEVEL = "ERROR";
+    private static String WARNING_LEVEL = "WARNING";
+    private static String DEBUG_LEVEL = "DEBUG";
+    private static String INFO_LEVEL = "INFO";
+
+    private static final Logger logger = Logger.getLogger(MSTestReportConverter.class.getName());
 
     MsTestLogger(TaskListener listener) {
         this.listener = listener;
+        this.configuredLevel = parseLevel(System.getProperty(HUDSON_PLUGINS_MSTEST_LEVEL));
     }
 
     static MsTestLogger getLogger() {
@@ -42,10 +51,34 @@ class MsTestLogger implements Serializable {
         String messageFormat = String
             .format("%s %s %s%n", MsTestLogger.prefix, level.getName(), format);
         if (listener != null) {
-            listener.getLogger().printf(messageFormat, args);
+            if(shouldLog(level)){
+                listener.getLogger().printf(messageFormat, args);
+            }
         } else {
-            Logger logger = Logger.getLogger(MSTestReportConverter.class.getName());
+            logger.setLevel(this.configuredLevel);
             logger.log(level, messageFormat, args);
+        }
+    }
+
+    private boolean shouldLog(Level level){
+        if(this.configuredLevel != null){
+            return level.intValue() >= configuredLevel.intValue();
+        } else {
+            return true;
+        }
+    }
+
+    public static Level parseLevel(String level) throws RuntimeException {
+        if(ERROR_LEVEL.equals(level)){
+            return Level.SEVERE;
+        } else if (WARNING_LEVEL.equals(level)){
+            return Level.WARNING;
+        } else if (DEBUG_LEVEL.equals(level)){
+            return Level.FINE;
+        } else if (INFO_LEVEL.equals(level)){
+            return Level.INFO;
+        } else {
+            throw new RuntimeException(String.format("Unknown [%s] level provided!", level));
         }
     }
 }
